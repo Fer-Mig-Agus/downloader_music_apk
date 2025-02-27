38a3b81f03f500babdd97b6a8910ec19a8953df9
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-02-27 00:01:45 -0300

    Modificacion de la ventanas, se agrego un popup

--- main.py
+++ main.py
@@ -26,6 +26,32 @@ class Main_window:
         self.window.iconbitmap(self.get_path_icon('logo.ico'))
         self.window.config(bg='sky blue')
 
+    def destroy_element(self,element:tk):
+        element.destroy()
+
+    def show_popup_notification(self, message:str,type_:str):
+
+        frame=tk.Frame(self.window, bg='lightgray')
+        frame.configure(width=200,height=100)
+        label=tk.Label(frame,text=message,bg='lightgray')
+        label.pack()
+        button=tk.Button(frame,width=50,height=20,bg='blue', text='Aceptar',command=lambda: self.destroy_element(frame))
+        button.pack()
+
+        frame.pack_propagate(False)
+        self.window.after(100, frame.pack)
+
+
+    def verify_input(self,input_):
+        value=input_.get()
+        print(value)
+        if value == "":
+            print('entro en el if')
+            self.show_popup_notification('Debes de buscar algo','Error')
+        elif not 'https://' in value:
+            self.show_popup_notification('Debe ser una URL','Error')
+
+
 
     def run(self):
         # with_input=int(self.width_screen-(self.width_screen/2))
@@ -37,44 +63,13 @@ class Main_window:
 
         width_input = int(int(self.width_screen / 10)/2)
         print(width_input)
-        input=tk.Entry(self.window)
-        input.configure(width=width_input)
-        input.pack()
-
+        input_=tk.Entry(self.window)
+        input_.configure(width=width_input)
+        input_.pack()
+        button=tk.Button(self.window,text='Buscar',command=lambda: self.verify_input(input_))
+        button.pack()
 
         self.window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Main window
-
 
 
 
